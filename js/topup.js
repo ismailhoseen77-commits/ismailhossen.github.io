06ec9eee56a942b31e11bf1720727e81b2d1f1d8
ismailhoseen77-commits <ismailhoseen77@gmail.com>
2026-08-17 00:49:18 +0600

    Create topup.js

---
+++ js/topup.js
@@ -0,0 +1,7 @@
+import { db, collection, addDoc } from "./firebase-init.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Top-up module loaded for mihadx.pro.bd");
+    // Free Fire & eFootball dynamic package logic here
+});
+
