c38edd48e8170e12fccb1a896c890714a4912431
ismailhoseen77-commits <ismailhoseen77@gmail.com>
2026-08-17 14:06:50 +0600

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,13 +1,14 @@
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyACc83rBZjnoZ8l-pZFIRqSs-fyVoiN6ck",
+  authDomain: "mihadx-pro-bd.firebaseapp.com",
+  projectId: "mihadx-pro-bd",
+  storageBucket: "mihadx-pro-bd.firebasestorage.app",
+  messagingSenderId: "478391400367",
+  appId: "1:478391400367:web:86012d35014ee7350a215a",
+  measurementId: "G-E8Y80PDVDD"
 };
 
-// Initialize Firebase
+// Initialize Firebase App
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
